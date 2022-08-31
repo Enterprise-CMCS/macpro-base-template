eb6752c50acd507afa9ec7cf96a365d22e7d4aa9
Mike Dial <michael.dial.4@gmail.com>
2022-08-31 14:44:27 -0400

    feat(initial release): Initial Release

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center" style="border-bottom: none;">mmdl-connectors</h1>
+<h3 align="center">A microservice sending CMS Medicaid Model Data Lab (MMDL) data to CMS Bigmac.</h3>
+<p align="center">
+  <a href="https://cmsgov.slack.com/archives/C0403M0D007">
+    <img alt="Slack" src="https://img.shields.io/badge/Slack-mmdl--connectors-purple.svg">
+  </a>
+  <a href="https://codeclimate.com/github/CMSgov/mmdl-connectors/maintainability">
+    <img src="https://api.codeclimate.com/v1/badges/53d1f826293cf113ba38/maintainability" />
+  </a>
+  <a href="https://dependabot.com/">
+    <img alt="Dependabot" src="https://badgen.net/badge/Dependabot/enabled/green?icon=dependabot">
+  </a>
+  <a href="https://github.com/prettier/prettier">
+    <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square">
+  </a>
+  <a href="https://github.com/semantic-release/semantic-release">
+    <img alt="semantic-release: angular" src="https://img.shields.io/badge/semantic--release-angular-e10079?logo=semantic-release">
+  </a>
+</p>
+
+## Overview
+
+The mmdl-connnectors project (oftened referenced in the context of this repository as just 'mmdl') is a microservice that streams Medicaid Data Lab (MMDL) data from it's Oracle database to CMS Bigmac's Kafka cluster in real time. It is powered by Kafka Connect and a JDBC connector.
+
+That's all for now; this project was just created!
+
+![Architecture Diagram](./.images/architecture.svg?raw=true)
+
+## Contributing
+
+Work items for mmdl-connectors are tracked in CMS' Jira. If you have access to our Jira, you can view mmdl-connectors related work there. While there's no dedicated mmdl-connectors product view in Jira yet, the [Platform Team board](https://qmacbis.atlassian.net/jira/software/c/projects/OY2/boards/216/backlog?selectedIssue=OY2-17657&epics=visible&issueLimit=100) is a good stand in.
+
+If you don't have access to Jira, would like access to Jira, or would like to discuss any enhancement, bug, idea, or question, please visit the [MMDL Connectors Slack channel](https://cmsgov.slack.com/archives/C0403M0D007). This is a public channel open to anyone in CMS Slack, and all input is welcome!
+
+## License
+
+[![License](https://img.shields.io/badge/License-CC0--1.0--Universal-blue.svg)](https://creativecommons.org/publicdomain/zero/1.0/legalcode)
+
+See [LICENSE](LICENSE) for full details.
---
+++ src/run.ts
@@ -0,0 +1,143 @@
+import yargs from "yargs";
+import * as dotenv from "dotenv";
+import LabeledProcessRunner from "./runner.js";
+import * as fs from "fs";
+import { ServerlessStageDestroyer } from "@stratiformdigital/serverless-stage-destroyer";
+import { SechubGithubSync } from "@stratiformdigital/security-hub-sync";
+
+// load .env
+dotenv.config();
+
+const runner = new LabeledProcessRunner();
+
+function touch(file: string) {
+  try {
+    const time = new Date();
+    fs.utimesSync(file, time, time);
+  } catch (err) {
+    fs.closeSync(fs.openSync(file, "w"));
+  }
+}
+
+async function frozenInstall(runner: LabeledProcessRunner, dir: string) {
+  await runner.run_command_and_output(
+    `${dir.split("/").slice(-1)} deps`,
+    ["yarn", "install", "--frozen-lockfile"],
+    dir
+  );
+}
+
+async function install_deps(runner: LabeledProcessRunner, dir: string) {
+  if (process.env.CI == "true") {
+    if (!fs.existsSync(`${dir}/node_modules`)) {
+      await frozenInstall(runner, dir);
+    }
+  } else {
+    if (
+      !fs.existsSync(`${dir}/.yarn_install`) ||
+      fs.statSync(`${dir}/.yarn_install`).ctimeMs <
+        fs.statSync(`${dir}/yarn.lock`).ctimeMs
+    ) {
+      await frozenInstall(runner, dir);
+      touch(`${dir}/.yarn_install`);
+    }
+  }
+}
+
+async function install_deps_for_services() {
+  var services = getDirectories("src/services");
+  for (let service of services) {
+    await install_deps(runner, `src/services/${service}`);
+  }
+}
+
+function getDirectories(path: string) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + "/" + file).isDirectory();
+  });
+}
+
+yargs(process.argv.slice(2))
+  .command("install", "install all service dependencies", {}, async () => {
+    await install_deps_for_services();
+  })
+  .command(
+    "deploy",
+    "deploy the project",
+    {
+      stage: { type: "string", demandOption: true },
+    },
+    async (options) => {
+      await install_deps_for_services();
+      await runner.run_command_and_output(
+        `SLS Compose Deploy`,
+        ["sls", "deploy", "--stage", options.stage],
+        "."
+      );
+    }
+  )
+  .command(
+    "test",
+    "run any available tests for an mmdl stage.",
+    {
+      stage: { type: "string", demandOption: true },
+    },
+    async (options) => {
+      await install_deps_for_services();
+      console.log(
+        `Here, we would run tests for ${options.stage}, but there are no tests yet!`
+      );
+    }
+  )
+  .command(
+    "destroy",
+    "destroy a stage in AWS",
+    {
+      stage: { type: "string", demandOption: true },
+      service: { type: "string", demandOption: false },
+      wait: { type: "boolean", demandOption: false, default: true },
+      verify: { type: "boolean", demandOption: false, default: true },
+    },
+    async (options) => {
+      let destroyer = new ServerlessStageDestroyer();
+      let filters = [
+        {
+          Key: "PROJECT",
+          Value: `${process.env.PROJECT}`,
+        },
+      ];
+      if (options.service) {
+        filters.push({
+          Key: "SERVICE",
+          Value: `${options.service}`,
+        });
+      }
+      await destroyer.destroy(`${process.env.REGION_A}`, options.stage, {
+        wait: options.wait,
+        filters: filters,
+        verify: options.verify,
+      });
+    }
+  )
+  .command(
+    "syncSecurityHubFindings",
+    "Syncs Sec Hub findings to GitHub Issues... usually only run by the CI system.",
+    {
+      auth: { type: "string", demandOption: true },
+      repository: { type: "string", demandOption: true },
+    },
+    async (options) => {
+      for (let region of [process.env.REGION_A, process.env.REGION_B]) {
+        var sync = new SechubGithubSync({
+          repository: options.repository,
+          auth: options.auth,
+          region: region,
+          accountNickname: "dev",
+        });
+        await sync.sync();
+      }
+    }
+  )
+  .strict() // This errors and prints help if you pass an unknown command
+  .scriptName("run") // This modifies the displayed help menu to show 'run' isntead of 'dev.js'
+  .demandCommand(1, "").argv; // this prints out the help if you don't call a subcommand
