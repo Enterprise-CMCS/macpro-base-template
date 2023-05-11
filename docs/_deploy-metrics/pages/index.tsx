82b3f5fe767bdea96e9b61b61c71bcfb8d1c75e5
Benjamin Paige <benjamin.paige@gmail.com>
2023-05-11 12:48:48 -0600

    trying something else

--- docs/_deploy-metrics/pages/index.tsx
+++ docs/_deploy-metrics/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { InferGetStaticPropsType } from "next";
-import { getAwsResources } from "../lib/getAwsResources";
-import * as UI from "@chakra-ui/react";
-import { Resources } from "../components/Resources";
+import { Cards } from "../components/Cards";
+import { getMeanTimeToRecover } from "../lib/getMeanTimeToRecover";
+import { getPrsToBranch } from "../lib/getPrsToBranch";
+import { getSuccessfulDeploys } from "../lib/getSuccessfulDeploys";
+import {
+  Divider,
+  Container,
+  Heading,
+  Text,
+  Select,
+  HStack,
+} from "@chakra-ui/react";
+import { LeadTimeForChanges } from "../components/LeadTimeForChanges";
+import { DeploymentFrequency } from "../components/DeploymentFrequency";
 import { octokitBranchesToUse } from "../lib/octokit";
 import { useState } from "react";
 import packageJson from "../../../package.json";
@@ -9,15 +20,21 @@ import packageJson from "../../../package.json";
 export const getStaticProps = async () => {
   const branchData: {
     [name: string]: {
-      resources: Awaited<ReturnType<typeof getAwsResources>>;
+      meanTimeToRecover: Awaited<ReturnType<typeof getMeanTimeToRecover>>;
+      prsToBranch: Awaited<ReturnType<typeof getPrsToBranch>>;
+      successfulDeploys: Awaited<ReturnType<typeof getSuccessfulDeploys>>;
     };
   } = {};
 
   for (const branch of octokitBranchesToUse!) {
-    const resources = await getAwsResources(branch);
+    const meanTimeToRecover = await getMeanTimeToRecover(branch);
+    const prsToBranch = await getPrsToBranch(branch);
+    const successfulDeploys = await getSuccessfulDeploys(branch);
 
     branchData[branch] = {
-      resources,
+      meanTimeToRecover,
+      prsToBranch,
+      successfulDeploys,
     };
   }
 
@@ -30,58 +47,73 @@ export const getStaticProps = async () => {
   };
 };
 
-const Aws = ({
+const Dora = ({
   branchData,
   repoName,
   branches,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const [selectedBranch, setSelectedBranch] = useState<string>(branches![0]);
 
-  const { resources } = branchData[selectedBranch];
+  const dataForBranch = branchData[selectedBranch];
 
   return (
-    <UI.Container centerContent maxW="8xl" pb="24">
-      <UI.Stack
-        w="100%"
-        direction={{ base: "column", md: "row" }}
-        justify="space-between"
-        p="10"
-        pb="4"
-      >
-        <div>
-          <UI.Heading as="h1" size={"lg"}>
-            AWS Resources
-          </UI.Heading>
-          <UI.Heading as="h2" size={"md"}>
-            {repoName}
-          </UI.Heading>
-        </div>
-        <UI.HStack>
-          <UI.Text flex={2}>Currently viewing data for</UI.Text>
-          <UI.Select
-            w="max-content"
-            value={selectedBranch}
-            onChange={(newValue) => {
-              setSelectedBranch(newValue.currentTarget.value);
-            }}
-          >
-            {branches?.map((branch, index) => (
-              <option key={index} value={branch}>
-                {branch.toUpperCase()}
-              </option>
-            ))}
-          </UI.Select>
-        </UI.HStack>
-      </UI.Stack>
-      <UI.Divider m={5} />
-      {resources && (
-        <Resources
-          data={resources}
-          downloadFileName={`${repoName}-${selectedBranch}-aws-resources.csv`}
-        />
-      )}
-    </UI.Container>
+    <Container centerContent>
+      <Heading as="h1">Deployment Metrics for {repoName}</Heading>
+      <HStack pt={"4"}>
+        <Text flex={2}>Currently viewing data for</Text>
+        <Select
+          flex={1}
+          maxW={"md"}
+          value={selectedBranch}
+          onChange={(newValue) =>
+            setSelectedBranch(newValue.currentTarget.value)
+          }
+        >
+          {branches?.map((branch, index) => (
+            <option key={index} value={branch}>
+              {branch.toUpperCase()}
+            </option>
+          ))}
+        </Select>
+      </HStack>
+      <Divider my={5} />
+      <Cards
+        cards={[
+          {
+            questionAnswer: `On average it takes ${dataForBranch.prsToBranch.averageTimeToMerge.toFixed(
+              2
+            )} hours`,
+            questionText: `How long does it take to get code successfully running in ${selectedBranch.toLowerCase()}?`,
+          },
+          {
+            questionAnswer: `${dataForBranch.successfulDeploys?.passedRuns.toString()} times per year`,
+            questionText: `How often is code deployed to ${selectedBranch.toLowerCase()}?`,
+          },
+          {
+            questionAnswer: `On average it takes ${dataForBranch.meanTimeToRecover} hours`,
+            questionText:
+              "How long does it take to recover from bad code being released?",
+          },
+          {
+            questionAnswer: `${(
+              (dataForBranch.successfulDeploys.failedRuns /
+                (dataForBranch.successfulDeploys.failedRuns +
+                  dataForBranch.successfulDeploys.passedRuns)) *
+              100
+            ).toFixed(2)} %`,
+            questionText:
+              "What percent of changes to the code result in a degraded service?",
+          },
+        ]}
+      />
+      <Divider my={4} />
+      <LeadTimeForChanges {...dataForBranch.prsToBranch} />
+      <DeploymentFrequency
+        selectedBranch={selectedBranch}
+        successfulDeploys={{ ...dataForBranch.successfulDeploys }}
+      />
+    </Container>
   );
 };
 
-export default Aws;
+export default Dora;
